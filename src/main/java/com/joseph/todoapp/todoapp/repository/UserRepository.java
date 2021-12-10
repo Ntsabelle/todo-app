7a6c8ecff569f04d0d64126d345567de14b2636f
Admin <Admin@hp820g4>
2021-12-10 12:42:55 +0200

    delete

--- src/main/java/com/joseph/todoapp/todoapp/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.joseph.todoapp.todoapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.joseph.todoapp.todoapp.entity.User;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-}
